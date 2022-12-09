9e8ad035fc94592660e6a0583ff3f267a6cb76b7
baktiaruuluerbol5@gmail.com <bantiaruuluerbol5@gmail.com>
2022-12-09 12:34:50 +0600

    Exponentiation

--- wer.js
+++ wer.js
@@ -1,2 +1,23 @@
 let a1 = 1 + 2;
 alert(a1);
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// alert(a + b + c);
+// let a = 2;
+// let b = 3;
+// let c = a + b;
+// alert(c);
+// let d = 4;
+// alert(c + d);
+//-----------Negative numbers---------------
+// let a = 1.5;
+// let b = 0.75;
+// alert(a + b);
+//---------------Remainder of the division------------------
+// let a = 13;
+// let b = 5;
+// alert(a % b);
+//--------------Exponentiation------------
+let a = 2 * 3 ** 2 * 3;
+alert(a);
