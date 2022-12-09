af4dbed9439706fdddf6ac75b13fedc61ed6637b
baktiaruuluerbol5@gmail.com <bantiaruuluerbol5@gmail.com>
2022-12-10 05:07:28 +0600

    Objects in JavaScript

--- wer.js
+++ wer.js
@@ -54,21 +54,64 @@
 // alert(1024 * 1024);
 // document.write(1024 * 1024);
 //--------------Arrays-----------------
-const arr = [1, 2, 3];
-console.log(arr.length);
-const arrr = ["a", "b", "c"];
-arrr[1] = 1;
-console.log(arrr);
-let a = [1, 2, 3];
-a[3] = 4;
-a[4] = 5;
-alert(a);
-let b = [];
-b[1] = "a";
-b[2] = "2";
-console.log(b.length);
-let c = [];
-c.push("a");
-c.push("b");
-c.push("c");
-console.log(c);
+// const arr = [1, 2, 3];
+// console.log(arr.length);
+// const arrr = ["a", "b", "c"];
+// arrr[1] = 1;
+// console.log(arrr);
+// let a = [1, 2, 3];
+// a[3] = 4;
+// a[4] = 5;
+// alert(a);
+// let b = [];
+// b[1] = "a";
+// b[2] = "2";
+// console.log(b.length);
+// let c = [];
+// c.push("a");
+// c.push("b");
+// c.push("c");
+// delete c[1];
+// console.log(c.length);
+//////////Objects in JavaScript----------------------
+let obj = {
+  1: "панидельник",
+  2: "вторник",
+  3: "Среда",
+  4: "Четверг",
+  5: "Пятница",
+  6: "Субота",
+  7: "Воскресение",
+};
+console.log(obj);
+const date = { год: 2022, месяц: 12, день: 11 };
+console.log(date);
+let obj1 = { x: 1, y: 2, z: 3 };
+obj1["x"] = 1;
+obj1["y"] = 4;
+obj1["z"] = 6;
+console.log(obj1);
+const obj2 = {};
+obj2["a"] = 1;
+obj2["b"] = 2;
+obj2["c"] = 3;
+console.log(obj2);
+// const obj3 = { 7: "a", 50: "b", 2: "c" };
+// const keys = Object.keys(obj3);
+// console.log(keys.length);
+const obj4 = { a: 2, b: 3, c: 3 };
+const key = ["a"];
+console.log(key);
+const obj5 = { name: "erbol", sourname: "baktiar" };
+const keys = Object.keys(obj5);
+console.log(keys);
+const obj6 = { a: 2, b: 3, c: 4 };
+
+console.log(typeof obj6["a"]);
+console.log(typeof { a: 1, b: 2, c: 3 });
+const arr = ["a", "b", "c"];
+ar = ["a", "b", "c"];
+console.log(arr);
+const arrr = [1, 2, 3, 4, 5];
+let res = arrr[1] + arrr[2];
+console.log(res);
