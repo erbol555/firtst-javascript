207ed9b851047ba6de8c1bc4b0d47b34a8b386c2
baktiaruuluerbol5@gmail.com <bantiaruuluerbol5@gmail.com>
2022-12-09 14:09:52 +0600

    prompt function

--- wer.js
+++ wer.js
@@ -1,45 +1,51 @@
-let a1 = 1 + 2;
-alert(a1);
-// let a = 10;
-// let b = 20;
-// let c = 30;
-// alert(a + b + c);
-// let a = 2;
-// let b = 3;
-// let c = a + b;
-// alert(c);
-// let d = 4;
-// alert(c + d);
-//-----------Negative numbers---------------
-// let a = 1.5;
-// let b = 0.75;
-// alert(a + b);
-//---------------Remainder of the division------------------
-// let a = 13;
-// let b = 5;
-// alert(a % b);
-//--------------Exponentiation------------
-let a = 2 * 3 ** 2 * 3;
-alert(a);
-//---------------Strings------
-let str = "baktiar uulu erbol";
-alert(str);
-//--------------String addition------------
-let text = "java";
-let text1 = "script";
-alert(text + text1);
-let name = "baktiaruuluerbol";
-alert(name.length);
-//----------------Multiline----------
-let ster = `a, 
-b, c`;
-alert(ster);
-let as = 10;
-let ass = 0;
-alert(10 / 0);
-//--------------------Convert to string
-let num = 12345;
-alert(num.length);
-let num1 = 12345;
-alert(String(num).length);
-console.log(num + num1);
+// let a1 = 1 + 2;
+// alert(a1);
+// // let a = 10;
+// // let b = 20;
+// // let c = 30;
+// // alert(a + b + c);
+// // let a = 2;
+// // let b = 3;
+// // let c = a + b;
+// // alert(c);
+// // let d = 4;
+// // alert(c + d);
+// //-----------Negative numbers---------------
+// // let a = 1.5;
+// // let b = 0.75;
+// // alert(a + b);
+// //---------------Remainder of the division------------------
+// // let a = 13;
+// // let b = 5;
+// // alert(a % b);
+// //--------------Exponentiation------------
+// let a = 2 * 3 ** 2 * 3;
+// alert(a);
+// //---------------Strings------
+// let str = "baktiar uulu erbol";
+// alert(str);
+// //--------------String addition------------
+// let text = "java";
+// let text1 = "script";
+// alert(text + text1);
+// let name = "baktiaruuluerbol";
+// alert(name.length);
+// //----------------Multiline----------
+// let ster = `a,
+// b, c`;
+// alert(ster);
+// let as = 10;
+// let ass = 0;
+// alert(10 / 0);
+// //--------------------Convert to string----------
+// let num = 12345;
+// alert(num.length);
+// let num1 = 12345;
+// alert(String(num).length);
+// console.log(num + num1);
+//-----------------Getting the characters of a string-------------
+let tun = "abcde";
+console.log(tun[tun.length - 2]);
+alert(0.1 * 0.23);
+let name = prompt("ведите имя");
+alert("erbol" + name);
